afdd17eedd6f7a19373f2d27b077cc9e7d6dfc1f
Esteban Ramirez <esteban941017@gmail.com>
2025-01-20 17:25:59 -0500

    added resources on aws

--- src/application/controller/WebhookController.ts
+++ src/application/controller/WebhookController.ts
@@ -30,4 +30,4 @@ routes.get('/verification', async (req, res) => {
 export default routes;
 
 //http://localhost:8000/meu-cofrinho/webhook/verification?hub.mode=subscribe&hub.challenge=1158201444&hub.verify_token=a45c0c73-1b66-40a3-a969-1aea5c433844
-//https://vhspygdzy2.execute-api.us-east-1.amazonaws.com/dev/meu-cofrinho/webhook/verification?hub.mode=subscribe&hub.challenge=1158201444&hub.verify_token=a45c0c73-1b66-40a3-a969-1aea5c433844
+//https://5g4szryhme.execute-api.us-east-1.amazonaws.com/dev/meu-cofrinho/webhook/verification?hub.mode=subscribe&hub.challenge=1158201444&hub.verify_token=a45c0c73-1b66-40a3-a969-1aea5c433844
